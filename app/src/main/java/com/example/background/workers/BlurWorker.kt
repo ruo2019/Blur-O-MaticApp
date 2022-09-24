393b3d630fad28c6929819ee4d4536cae4dbb64d
ruo2019 <ruo2019@outlook.com>
2022-09-24 11:37:49 -0700

    Override and implement doWork()

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -1,8 +1,27 @@
 package com.example.background.workers
 
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.background.R
 
+private const val TAG = "BlurWorker"
 class BlurWorker(ctx: Context, params: WorkerParameters): Worker(ctx, params) {
+    override fun doWork(): Result {
+        val appContext = applicationContext
+        makeStatusNotification("Blurring image", appContext)
+        return try {
+            val picture =
+                BitmapFactory.decodeResource(appContext.resources, R.drawable.android_cupcake)
+            blurBitmap(picture, appContext)
+            val uri = writeBitmapToFile(appContext, picture)
+            makeStatusNotification(uri.toString(), appContext)
+            Result.success()
+        } catch (throwable:Throwable) {
+            Log.e(TAG, "Error applying blur")
+            Result.failure()
+        }
+    }
 }
