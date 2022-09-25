796b3faab11844d75b124484308882b971dd289c
ruo2019 <ruo2019@outlook.com>
2022-09-24 17:44:01 -0700

    Blur the given URI

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,6 +2,8 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
@@ -15,15 +17,21 @@ class BlurWorker(ctx: Context, params: WorkerParameters): Worker(ctx, params) {
         val resourceUri = inputData.getString(KEY_IMAGE_URI)
         makeStatusNotification("Blurring image", appContext)
         return try {
-            val picture =
-                BitmapFactory.decodeResource(appContext.resources, R.drawable.android_cupcake)
+            if (TextUtils.isEmpty(resourceUri)) {
+                Log.e(TAG, "Invalid input uri")
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = appContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(resolver.openInputStream(Uri.parse(resourceUri)))
             val output = blurBitmap(picture, appContext)
             val outputUri = writeBitmapToFile(appContext, output)
 
-            makeStatusNotification("Output is $outputUri", appContext)
             Result.success()
         } catch (throwable:Throwable) {
             Log.e(TAG, "Error applying blur")
+            throwable.printStackTrace()
             Result.failure()
         }
     }
